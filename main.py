07b5446f24f3798236b24c1a3f76ca1c24c0207f
Jacob Chestnut <jacob.chestnut@fbitn.com>
2025-03-01 17:15:30 -0600

    add post, read, delete functionality to user objects

--- main.py
+++ main.py
@@ -20,6 +20,21 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 @app.post("/users/", status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserBase, db: db_dependency):
-    db_user = models.User(**user.model_dump())
-    db.add(db_user)
+    user = models.User(**user.model_dump())
+    db.add(user)
+    db.commit()
+
+@app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
+async def read_user(user_id: int, db: db_dependency):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail='User not found.')
+    return db_user
+
+@app.delete("/users/{user_id}", status_code=status.HTTP_200_OK)
+async def delete_user(user_id: int, db: db_dependency):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail='User was not found.')
+    db.delete(db_user)
     db.commit()
